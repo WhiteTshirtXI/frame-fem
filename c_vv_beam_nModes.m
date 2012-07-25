fe9741b43b82db64490cba42a4e8daca5285006a
Felix Langfeldt <felix.langfeldt@haw-hamburg.de>
2012-07-25 10:20:02 +0200

    Replace VV-scripts with VV-classes

---
+++ c_vv_beam_nModes.m
@@ -0,0 +1,440 @@
+classdef c_vv_beam_nModes < handle
+%C_VV_BEAM_NMODES - Verification class: beam normal modes
+% This class is for verifying the fem system using the free vibrations
+% of a single beam which has exact analytical solutions as a reference.
+% The example beam properties are taken from:
+% FAHY (2007): Sound and Structural Vibration: Radiation, Transmission
+% and Response. Elsevier. Pp. 460-469.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% c_vv_beam_nModes.m
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Class definition
+%
+% Constant Properties :
+%    H   - beam cross section height (m)
+%    W   - beam cross section width (m)
+%    L   - beam length (m)
+%    A   - beam angle (rad)
+%    RHO - density (kg/m^3)
+%    E   - young's modulus (N/m^2)
+%    NEL - element density on beam (1/m)
+%    NM  - default maximum modes number
+%
+% Properties :
+%    rhoA     - mass
+%    EA       - axial stiffness
+%    EI       - bending stiffness
+%
+%    nodes    - beam nodes
+%    beam     - beam
+%    frame    - frame class
+%    fem      - fem class
+%
+%    nSpprted - simply supported nodes
+%    nClamped - clamped nodes
+%
+% Methods :
+%    c_vv_beam_nModes - constructor
+%
+%    studyRefine      - refinement study
+%    studyAlpha       - beam angle study
+%    vv               - perform vv-study
+%
+%    initFrame        - initialize frame class
+%    initFEA          - initialize fe-analysis 
+%    initNodes        - initialize nodes
+%    initBeam         - initialize beam specification
+%    initBCs          - initialize boundary conditions
+%
+%    eigFa            - analytical solution for eigenfrequencies
+%
+%    txtOut           - text output
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Author        : Felix Langfeldt
+%                 felix.langfeldt@haw-hamburg.de
+%
+% Creation Date : 2012-07-03 15:00 CEST
+% Last Modified : 2012-07-25 10:15 CEST
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    % CONSTANT PROPERTIES %
+    properties (Constant)
+
+        % DEFAULT BEAM PROPERTIES
+
+        % square cross section: height and width (m)
+        H =   5e-3;
+        W =  20e-3;
+
+        % beam length (m)
+        L = 500e-3;
+
+        % beam angle (rad)
+        A = 0;
+
+        % density (kg/m^3)
+        RHO = 2700;
+
+        % young's modulus (N/m^2)
+        E = 71000e6;
+
+        % DEFAULT FEM PROPERTIES
+        
+        % element density on beam (1/m)
+        NEL = 10;
+
+        % default maximum modes number
+        NM = 4;
+
+    end
+
+    % PRIVATE PROPERTIES %
+    properties (SetAccess = private)
+
+        % DERIVED PROPERTIES
+
+        % mass
+        rhoA;
+        
+        % stiffnesses
+        EA;
+        EI;
+
+        % FEM PROPERTIES
+
+        % beam nodes
+        nodes;
+        % beam
+        beam;
+        % frame class
+        frame;
+        % fem class
+        fem;
+
+        % simply supported nodes
+        nSpprted;
+        % clamped nodes
+        nClamped;
+
+    end
+
+    % METHODS
+    methods
+
+        % CONSTRUCTOR
+        function s = c_vv_beam_nModes()
+
+            s.rhoA = s.RHO*s.H*s.W;
+            s.EA = s.E*s.H*s.W;
+            s.EI = s.E*s.H^3*s.W/12;
+
+            s.initNodes();
+            s.initBeam();
+
+            s.nSpprted = [2];
+            s.nClamped = [1];
+
+        end
+
+        % REFINEMENT STUDY
+        %
+        % Inputs:
+        %   p_n - maximum number of modes (OPTIONAL)
+        %   p_r - number of refinement levels (OPTIONAL)
+        function [f,d] = studyRefine( s, p_n, p_r )
+
+            if ~exist('p_n','var'), p_n = s.NM ; end
+            if ~exist('p_r','var'), p_r = 1 ; end
+
+            % maximum node number
+            nMax = p_n;
+
+            % calculate analytical eigenfrequency vector
+            f = s.eigFa(nMax);
+
+            % pre-allocate results matrix (relative difference between
+            % numeric and analytical solution)
+            d = zeros(p_n,p_r);
+
+            % perfom refinement study
+            for r = 1:p_r
+
+                % initialize refined beam and FEA-system
+                s.initBeam(r);
+                s.initFEA();
+
+                % if neccessary, correct maximum node number
+                nMax = min(nMax, numel(s.fem.nodes.idxFreeDOFs));
+
+                % calculate eigenfrequencies of discretized system and
+                % compute difference to analytical solution
+                d(1:nMax,r) = sort(s.fem.eigF(nMax))./f(1:nMax)-1;
+
+            end
+
+            % reset beam and FEA-system
+            s.initBeam();
+            s.initFEA();
+
+            % return results
+            f = f(1:nMax);
+            d = d(1:nMax,:);
+
+        end
+
+        % BEAM ANGLE STUDY
+        %
+        % Inputs:
+        %   p_a - angle list
+        %   p_n - maximum number of modes (OPTIONAL)
+        %   p_r - number of refinement levels (OPTIONAL)
+        function [f,d] = studyAlpha( s, p_a, p_n, p_r )
+
+            if ~exist('p_a','var'), p_a = s.A ; end
+            if ~exist('p_n','var'), p_n = s.NM ; end
+            if ~exist('p_r','var'), p_r = 1 ; end
+
+            % pre-allocate results matrix (relative difference between
+            % numeric and analytical solution)
+            d = zeros(p_n,p_r,numel(p_a));
+
+            % run through all specifiec angles
+            for ia = 1:numel(p_a)
+
+                a = p_a(ia);
+            
+                % change nodes
+                s.initNodes(a);
+
+                % perform refinement study
+                [f,dTmp] = s.studyRefine(p_n, p_r);
+
+                d(1:numel(f),:,ia) = dTmp;
+
+                ia = ia + 1;
+
+            end
+
+            d = d(1:numel(f),:,:);
+
+        end
+
+        % PERFORM VV-STUDY
+        function s = vv( s )
+
+            % VV OPTIONS
+            % select random angles
+            alpha = rand(10,1)*2*pi;
+
+            % number of modes
+            nm = 8;
+
+            % number of refinement levels
+            nr = 6;
+
+            [f,d] = s.studyAlpha( alpha, nm, nr );
+
+            % check if the standard deviation in the results exceeds a
+            % specific tolerance
+            dev = max(max(std(d,0,3)));
+            devTol = sqrt(eps);
+
+            if dev > devTol
+
+                s.txtOut(['*** WARNING : results not independent '...
+                          'of beam angle!\n  -> dev = '...
+                          sprintf('%8.2e > %8.2e', [dev devTol])]);
+
+            else
+
+                s.txtOut(['Max. standard deviation in beam angle '...
+                          'study: '...
+                          sprintf('%8.2e < %8.2e', [dev devTol]) ...
+                          '\n  -> OK!\n']);
+
+                % plot grid convergence figure
+                figure;
+                loglog(2.^[0:nr-1]*s.NEL,abs(mean(d,3)).','-*');
+                title 'Refinement study - eigenfrequencies of a beam'
+                xlabel 'Number of elements'
+                ylabel 'Error'
+                legend(num2str(f,'%7.1f Hz'));
+
+            end
+
+        end
+
+        % INIALIZE FRAME CLASS
+        function s = initFrame(s)
+
+            % initialize frame-definition class
+            s.frame = c_frame_def(s.nodes);
+
+            % add the beams to frame-class
+            s.frame.addBeam(s.beam);
+
+        end
+
+        % INITIALIZE FE-ANALYSIS
+        function s = initFEA(s)
+
+            s.initFrame();
+            s.initBCs();
+            s.fem = s.frame.discretize();
+
+        end
+
+        % INITIALIZE NODES
+        %
+        % Inputs:
+        %   p_a - beam angle (OPTIONAL)
+        function s = initNodes( s, p_a )
+
+            if nargin < 2
+                p_a = s.A;
+            end
+
+            s.nodes = [0 0 ; cos(p_a) sin(p_a)]*s.L;
+
+        end
+
+        % INITIALIZE BEAM SPECIFICATION
+        %
+        % Inputs:
+        %   p_r - refinement level (OPTIONAL)
+        function s = initBeam( s, p_r )
+
+            if nargin < 2
+                p_r = 1;
+            end
+            
+            s.beam = [ 1 2 s.rhoA s.EA s.EI s.NEL*2^(p_r-1) ];
+
+        end
+
+        % INITIALIZE BOUNDARY CONDITIONS
+        function s = initBCs(s)
+
+            % initialize simply supported boundaries
+            s.frame.nodeBC_spprted(s.nSpprted);
+
+            % initialize clamped boundaries
+            s.frame.nodeBC_clamped(s.nClamped);
+
+        end
+
+
+        % ANALYTICAL SOLUTION FOR EIGENFREQUENCIES
+        %
+        % Inputs:
+        %   p_n - number of desired eigenfrequencies
+        function f = eigFa( s, p_n )
+
+            % REFERENCE:
+            % YOUNG, D. and R. FELGAR: Tables of characteristic
+            % functions representing normal modes of vibration of a
+            % beam. Engineering Research Series. University of Texas,
+            % 1949.
+            
+            % pre-allocate frequency vector
+            f = zeros(p_n,1);
+
+            % get longitudinal modes eigenfrequencies according to
+            % boundary conditions
+            if numel(s.nSpprted) + numel(s.nClamped) == 1
+                
+                % CASE 1 : one-sidedly fixed rod
+                f_l = inline('(2*(1:n).''-1)*pi/2');
+
+            else
+
+                % CASE 2 : both sides fixed or free
+                f_l = inline('(1:n).''*pi');
+
+            end
+
+            % get bending modes eigenfrequencies according to boundary
+            % conditions
+            switch numel(s.nSpprted)
+
+                % CASE 1 : supported-supported beam
+                case 2
+
+                    % inline function for n bending eigenfrequencies
+                    f_b = inline('((1:n).''*pi).^2');
+
+                % CASE 2 : clamped-supported or free-supported beam
+                case 1
+
+                    f_b = inline(['[  3.92660230 ;' ...
+                                  '   7.06858275 ;' ...
+                                  '  10.21017613 ;' ...
+                                  '  13.35176878 ;' ...
+                                  '  16.49336143 ;' ...
+                                  '  ((4*(6:n).''+1)*pi/4) ].^2']);
+
+                % no simply supported bc
+                case 0
+
+                    switch numel(s.nClamped)
+
+                        % CASE 3 : clamped-free beam
+                        case 1
+
+                            f_b = inline(['[  1.87510410 ;' ...
+                                          '   4.69409113 ;' ...
+                                          '   7.85475743 ;' ...
+                                          '  10.99554074 ;' ...
+                                          '  14.13716839 ;' ...
+                                          ' ((2*(6:n).''-1)*pi/2)' ...
+                                          ' ].^2']);
+
+                        % CASE 4 : clamped-clamped or free-free beam
+                        otherwise
+
+                            f_b = inline(['[  4.73004080 ;' ...
+                                          '   7.85320460 ;' ...
+                                          '  10.99560730 ;' ...
+                                          '  14.13716550 ;' ...
+                                          '  17.27875960 ;' ...
+                                          '  ((2*(6:n).''+1)*pi/2)' ...
+                                          ' ].^2']);
+
+                    end
+
+            end
+
+            % rigid body translations (f = 0)
+            f_rb = [];
+            if numel(s.nSpprted) == 1 & numel(s.nClamped) == 0
+                f_rb = zeros(1,1);
+            elseif numel(s.nSpprted) == 0 & numel(s.nClamped) == 0
+                f_rb = zeros(3,1);
+            end
+
+            % create analytical eigenfrequencies vector
+            f = sort([ f_rb ;
+                       f_l(p_n)*sqrt(s.EA/s.rhoA) ;                  ...
+                       f_b(p_n)*sqrt(s.EI/s.rhoA)/s.L ])/(2*pi*s.L);
+            f = f(1:p_n);
+
+        end
+
+        % TEXT OUTPUT
+        %
+        % Inputs:
+        %   p_t - text string
+        function s = txtOut(s, p_t)
+
+            fprintf(p_t);
+
+        end
+        
+    end
+
+end
---
+++ c_vv_beam_sInf.m
@@ -0,0 +1,422 @@
+classdef c_vv_beam_sInf < handle
+%C_VV_BEAM_SINF - Verification class: semi-infinite beam
+% This class is for verifying the fem system using the forced vibrations
+% of a semi-infinite beam which has exact analytical solutions as a
+% reference. The example beam properties are taken from:
+% WANG, C. and J.C.S. LAI (2000): Modelling the Vibration Behaviour of
+% Infinite Structures By FEM. Journal of Sound and Vibration 229(3),
+% pp. 453-466.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% c_vv_beam_sInf.m
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Class definition
+%
+% Constant Properties :
+%    H   - beam cross section height (m)
+%    W   - beam cross section width (m)
+%    L   - beam length (m)
+%    A   - beam angle (rad)
+%    RHO - density (kg/m^3)
+%    E   - young's modulus (N/m^2)
+%    OM  - excitation angular frequency (rad/s)
+%    F   - excitation force amplitudes (N)
+%    NEL - element density on beam (1/m)
+%
+% Properties :
+%    omega    - excitation frequency
+%    l        - length
+%
+%    rhoA     - mass
+%    EA       - axial stiffness
+%    EI       - bending stiffness
+%
+%    nodes    - beam nodes
+%    beam     - beam
+%    frame    - frame class
+%    fem      - fem class
+%
+%    nInfinit - infinite element nodes
+%
+% Methods :
+%    c_vv_beam_sInf - constructor
+%
+%    studyRefine    - refinement study
+%    studyLength    - beam length study
+%    vv             - perform vv-study
+%
+%    initFrame      - initialize frame class
+%    initFEA        - initialize fe-analysis 
+%    initNodes      - initialize nodes
+%    initBeam       - initialize beam specification
+%    initBCs        - initialize boundary conditions
+%    initForces     - initialize nodal forces
+%
+%    dpm            - calculate driving point mobility
+%    dpmA           - analytical solution for driving point mobility
+%                     matrix
+%
+%    txtOut         - text output
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Author        : Felix Langfeldt
+%                 felix.langfeldt@haw-hamburg.de
+%
+% Creation Date : 2012-07-19 10:46 CEST
+% Last Modified : 2012-07-25 10:15 CEST
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    % CONSTANT PROPERTIES %
+    properties (Constant)
+
+        % DEFAULT BEAM PROPERTIES
+
+        % square cross section: height and width (m)
+        H =   1e-3;
+        W =  10e-3;
+
+        % beam length (m)
+        L = 200e-3;
+
+        % beam angle (rad)
+        A = 0;
+
+        % density (kg/m^3)
+        RHO = 7860;
+
+        % young's modulus (N/m^2)
+        E = 210000e6;
+
+        % excitation angular frequency (rad/s)
+        OM = 100*2*pi;
+
+        % excitation force amplitudes (N)
+        F = [0 ; 1 ; 0];
+
+        % DEFAULT FEM PROPERTIES
+        
+        % element density on beam (1/m)
+        NEL = 10;
+
+    end
+
+    % PRIVATE PROPERTIES %
+    properties (SetAccess = private)
+
+        % excitation frequency
+        omega;
+
+        % length
+        l;
+
+        % DERIVED PROPERTIES
+
+        % mass
+        rhoA;
+        
+        % stiffnesses
+        EA;
+        EI;
+
+        % FEM PROPERTIES
+
+        % beam nodes
+        nodes;
+        % beam
+        beam;
+        % frame class
+        frame;
+        % fem class
+        fem;
+
+        % infinite element nodes
+        nInfinit;
+
+    end
+
+    % METHODS
+    methods
+
+        % CONSTRUCTOR
+        function s = c_vv_beam_sInf()
+
+            s.rhoA = s.RHO*s.H*s.W;
+            s.EA = s.E*s.H*s.W;
+            s.EI = s.E*s.H^3*s.W/12;
+
+            s.l = s.L;
+            s.omega = s.OM;
+
+            s.initNodes();
+            s.initBeam();
+
+            s.nInfinit = [2];
+
+        end
+
+        % REFINEMENT STUDY
+        %
+        % Inputs:
+        %   p_r - number of refinement levels (OPTIONAL)
+        function [dpmRef,dpmD] = studyRefine( s, p_r )
+
+            if ~exist('p_r','var'), p_r = 1 ; end
+
+            % calculate real part of analytical driving point moblity
+            % matrix
+            dpmRef = real(s.dpmA(s.omega));
+
+            % pre-allocate results matrix (relative difference between
+            % numeric and analytical solution)
+            dpmD = zeros(3,3,p_r);
+
+            % perfom refinement study
+            for r = 1:p_r
+
+                % initialize refined beam
+                s.initBeam(r);
+
+                % calculate real part of driving point mobility matrix
+                % and compute difference to analytical solution
+                dpmD(:,:,r) = max(real(s.dpm()),eps)./max(dpmRef,eps)-1;
+
+            end
+
+            % reset beam and FEA-system
+            s.initBeam();
+            s.initFEA();
+
+        end
+
+        % BEAM LENGTH STUDY
+        %
+        % Inputs:
+        %   p_l - number of length increments (OPTIONAL)
+        %   p_r - number of refinement levels (OPTIONAL)
+        function [dpmRef,dpmD] = studyLength( s, p_l, p_r )
+
+            if ~exist('p_l','var'), p_l = 1 ; end
+            if ~exist('p_r','var'), p_r = 1 ; end
+
+            % calculate real part of analytical driving point moblity
+            % matrix
+            dpmRef = real(s.dpmA(s.omega));
+
+            % pre-allocate results matrix (relative difference between
+            % numeric and analytical solution)
+            dpmD = zeros(3,3,p_r,p_l);
+
+            % perfom length incrementing
+            for l = 1:p_l
+
+                % increase beam length
+                s.l = s.L*2.^(l-1);
+
+                % initialize nodes
+                s.initNodes();
+
+                % perform refinement study
+                [~,dpmD(:,:,:,l)] = s.studyRefine(p_r);
+
+            end
+
+            % reset beam length
+            s.l = s.L;
+            s.initNodes();
+            s.initBeam();
+            s.initFEA();
+
+        end
+
+        % PERFORM VV-STUDY
+        function s = vv( s )
+
+            % VV OPTIONS
+            % number of length increments
+            nl = 4;
+
+            % number of refinement levels
+            nr = 3;
+
+            % calculate number of subplots
+            nPltRow = ceil(sqrt(nl));
+            nPltCol = ceil(nl/nPltRow);
+
+            % do beam length study
+            [ref,d] = s.studyLength( nl, nr );
+
+            % create parent figure
+            figure;
+            title 'Refinement study - semi infinite beam'
+
+            % crate subfigure for each length increment
+            for jl = 1:nl
+
+                % reshape results difference matrix for nicer plotting
+                dPlot = abs(reshape(d(:,:,:,jl),9,nr)).';
+
+                % plot grid convergence subfigure
+                subplot(nPltRow,nPltCol,jl);
+                loglog(2.^[0:nr-1]*s.NEL,dPlot(:,[1 5 6 8 9]),'-*');
+                title(sprintf('Beam length %.2f m', s.L*2.^(jl-1)));
+                xlabel 'Element density (1/m)';
+                ylabel 'Relative error';
+
+            end
+            
+            legend({'\epsilon(Y_{uN})',...
+                   '\epsilon(Y_{wQ})',...
+                   '\epsilon(Y_{{\beta}Q})',...
+                   '\epsilon(Y_{wM})',...
+                   '\epsilon(Y_{{\beta}M})'},...
+                   'Position',[0.85 0.4 0.09 0.26])
+
+        end
+
+        % INIALIZE FRAME CLASS
+        function s = initFrame(s)
+
+            % initialize frame-definition class
+            s.frame = c_frame_def(s.nodes);
+
+            % add the beams to frame-class
+            s.frame.addBeam(s.beam);
+
+        end
+
+        % INITIALIZE FE-ANALYSIS
+        function s = initFEA(s)
+
+            s.initFrame();
+            s.initBCs();
+            s.initForces();
+            s.fem = s.frame.discretize();
+
+        end
+
+        % INITIALIZE NODES
+        %
+        % Inputs:
+        %   p_a - beam angle (OPTIONAL)
+        function s = initNodes( s, p_a )
+
+            if nargin < 2
+                p_a = s.A;
+            end
+
+            s.nodes = [0 0 ; cos(p_a) sin(p_a)]*s.l;
+
+        end
+
+        % INITIALIZE BEAM SPECIFICATION
+        %
+        % Inputs:
+        %   p_r - refinement level (OPTIONAL)
+        function s = initBeam( s, p_r )
+
+            if nargin < 2
+                p_r = 1;
+            end
+            
+            s.beam = [ 1 2 s.rhoA s.EA s.EI s.NEL*2^(p_r-1) ];
+
+        end
+
+        % INITIALIZE BOUNDARY CONDITIONS
+        %
+        % Inputs:
+        %   p_o - excitation frequency (OPTIONAL)
+        %
+        function s = initBCs(s, p_o)
+
+            if nargin < 2
+                p_o = s.OM;
+            end
+
+            s.omega = p_o;
+
+            % initialize infinite element boundaries
+            s.frame.nodeBC_infinite(s.nInfinit, s.omega);
+
+        end
+
+        % INITIALIZE NODAL FORCES
+        %
+        % Inputs:
+        %   p_f - force vector (OPTIONAL)
+        function s = initForces(s, p_f)
+
+            if nargin < 2
+                p_f = s.F;
+            end
+
+            s.frame.addHarmonicForce(1, p_f);
+
+        end
+
+        % CALCULATE DRIVING POINT MOBILITY
+        function dpm = dpm( s )
+
+            % initialize driving point mobility matrix
+            dpm = zeros(3);
+
+            % run through three different load cases where a single unit
+            % force is applied at the first beam node cycling through
+            % normal, transversal and moment loading.
+            
+            jf = 0;
+
+            for f = eye(3)
+
+                jf = jf + 1;
+
+                s.initFrame();
+                s.initBCs();
+                s.initForces(f);
+                s.fem = s.frame.discretize();
+
+                % calculate complex velocity amplitudes
+                v = 1i*s.omega*s.fem.harmonicAnalysis(s.omega);
+
+                % calculate driving point mobilities (driving point =
+                % first node)
+                dpm(:,jf) = v(:,1);
+
+            end
+
+        end
+
+
+        % ANALYTICAL SOLUTION FOR DRIVING POINT MOBILITY MATRIX
+        %
+        % Inputs:
+        %   p_o - excitation frequency
+        function dpm = dpmA( s, p_o )
+
+            % wave numbers
+            kL = p_o*sqrt(s.rhoA/s.EA);
+            kB = sqrt(p_o)*(s.rhoA/s.EI)^.25;
+
+            % driving point mobility matrix according to beam theory
+            dpm = [ 1./(s.EA*kL) 0 0  ;
+                    0 (1-1i)./(s.EI*kB.^3) (1-2i)./(s.EI*kB.^2) ;
+                    0 1./(s.EI*kB.^2)      (1-1i)./(s.EI*kB) ].*p_o;
+
+        end
+
+        % TEXT OUTPUT
+        %
+        % Inputs:
+        %   p_t - text string
+        function s = txtOut(s, p_t)
+
+            fprintf(p_t);
+
+        end
+        
+    end
+
+end
